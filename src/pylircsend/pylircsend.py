a3bacf51c41ae3e5003bd6214566562c9bab3168
Andreas <andreas@a-netz.de>
2015-09-05 09:10:11 +0800

    Added support for sending ir data (yet untested).

--- src/pylircsend/pylircsend.py
+++ src/pylircsend/pylircsend.py
@@ -3,6 +3,8 @@ import array
 from fcntl import ioctl
 import os
 
+from gencmds import get_cmd_data
+
 
 dev_file='/dev/lirc0'
 
@@ -10,6 +12,12 @@ LIRC_SET_SEND_CARRIER = 13
 
 
 def set_carrier(fd, freq):
+    '''Set LIRC carrier frequency for sending.
+
+    :param fd: File descriptor of lirc device file.
+    :param freq: Carrier frequency to set.
+    '''
+    
     result = ioctl(fd, LIRC_SET_SEND_CARRIER, int(freq))
 
     if(result < 0):
@@ -19,7 +27,19 @@ def set_carrier(fd, freq):
     else:
         msg = "Set carrier frequency to {0}Hz."
         logging.info(msg.format(freq))
+
+
+def send_buffer(fd, data):
     
+    retval = os.write(fd, buffer)
+    
+    if retval == len(buffer):
+        msg = "Wrote {0} bytes of data to device."
+        logging.info(msg.format(retval))
+    else:
+        msg = "Failed to write data completely to device (return value {0})."
+        logging.error(msg.format(retval))
+
 
 def main():
 
@@ -28,11 +48,11 @@ def main():
 
     set_carrier(fd, 38000)
 
+    data = get_cmd_data()
+    send_buffer(data)
     
     os.close(fd)
 
-    # a = array.array('I', [1,2,3,4]).tobytes()
-    
 
 if __name__ == '__main__':
     main()
